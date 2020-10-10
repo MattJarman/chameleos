b09602f87f3d524c882478a60100252a3313845b
Matthew <mattjarman22@outlook.com>
2020-10-10 18:03:33 +0100

    Create development config, Add axios, Create HttpClient

---
+++ src/modules/HttpClient.ts
@@ -0,0 +1,32 @@
+import Axios, { AxiosError, AxiosInstance, AxiosResponse } from 'axios'
+
+declare module 'axios' {
+    // eslint-disable-next-line
+    interface AxiosResponse<T = any> extends Promise<T> {}
+}
+
+abstract class HttpClient {
+    protected readonly instance: AxiosInstance
+
+    protected constructor(baseURL: string) {
+        this.instance = Axios.create({
+            baseURL
+        })
+
+        this._initialiseResponseInterceptor()
+    }
+
+    private _initialiseResponseInterceptor() {
+        this.instance.interceptors.response.use(
+            this._handleResponse,
+            this._handleError
+        )
+    }
+
+    private _handleResponse = ({ data }: AxiosResponse) => data
+
+    protected _handleError = (error: AxiosError): Promise<AxiosError> =>
+        Promise.reject(error)
+}
+
+export default HttpClient
